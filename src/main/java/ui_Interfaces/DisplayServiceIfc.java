4f286105a9b08ba324660a44a5f589a6861d7c06
TheBeacon95 <30535313+TheBeacon95@users.noreply.github.com>
2024-08-03 15:33:07 +0200

    Decreased the FPS and made sure drawing is done before update

--- src/main/java/ui_Interfaces/DisplayServiceIfc.java
+++ src/main/java/ui_Interfaces/DisplayServiceIfc.java
@@ -8,6 +8,16 @@ import common.ServiceIfc;
  */
 public interface DisplayServiceIfc extends ServiceIfc {
     void draw();
-    
+
     void attachKeyHandler(KeyHandlerIfc keyHandler);
+
+    /**
+     * Shows if the Game is currently busy drawing.
+     * @return true: busy - false: waiting.
+     */
+//    boolean isDrawing();
+
+    void waitForDrawing();
+
+    void notifyDrawingDone();
 }
